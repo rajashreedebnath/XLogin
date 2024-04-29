d198825ff61723bdc09f9e587b687fecf26d69db
rajashreedebnath <rajashreedebnath29@gmail.com>
2024-04-30 00:04:00 +0530

    Test

--- login/src/App.js
+++ login/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import react, { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+export default function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    if( username === 'user' && password === 'password') {
+
+      setMessage('Welcome, user!')
+      setUsername('');
+      setPassword('');
+    }
+    else {
+      setMessage('Invalid username or password')
+    }
+  }
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login">
+
+
+      <h1>Login Page</h1>
+
+
+      {message && (
+        <div className='message'>
+          {message === 'Welcome, user!' ? message: 'Invalid username or password'}
+        </div>
+      )}
+
+
+
+      {!message || message === 'Invalid username or password' ? (
+
+        <form onSubmit={handleSubmit}>
+
+          <div className='form_input'>
+
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id='username'
+              value={username}
+              placeholder='Username'
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+
+          </div>
+
+          <div className='form_input'>
+
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id='password'
+              value={password}
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+
+      ) : null}
+      
     </div>
   );
 }
 
-export default App;
